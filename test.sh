76d8d13c4557bc831036b4a490cac12d241526e3
Yogesh Dhamija <ydhamija96@gmail.com>
2022-02-20 08:58:45 -0600

    test exit code fails if tests fail

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+any_failed=0
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
 
 for test_folder in test/*; do
     vim \
@@ -8,12 +13,14 @@ for test_folder in test/*; do
         -s ./${test_folder}/when.vim \
         ./${test_folder}/given.txt
 
-    RED='\033[0;31m'
-    GREEN='\033[0;32m'
-    NO_COLOR='\033[0m'
     if cmp --silent -- ./${test_folder}/expected.txt ./${test_folder}/actual.txt; then
         echo -e "${GREEN}${test_folder}: PASS${NO_COLOR}"
     else
-        echo -e "${GREEN}${test_folder}: FAIL${NO_COLOR}"
+        echo -e "${RED}${test_folder}: FAIL${NO_COLOR}"
+        any_failed=1
     fi
 done;
+
+if [ ${any_failed} -eq 1 ]; then 
+    exit 1
+fi
