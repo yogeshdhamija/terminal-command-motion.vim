b59b6ad3a77b5c0fc2e74e62dcde593cffe52897
Yogesh Dhamija <ydhamija96@gmail.com>
2022-02-20 08:53:27 -0600

    test output is colored

--- test.sh
+++ test.sh
@@ -8,9 +8,12 @@ for test_folder in test/*; do
         -s ./${test_folder}/when.vim \
         ./${test_folder}/given.txt
 
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    NO_COLOR='\033[0m'
     if cmp --silent -- ./${test_folder}/expected.txt ./${test_folder}/actual.txt; then
-        echo "${test_folder}: PASS"
+        echo -e "${GREEN}${test_folder}: PASS${NO_COLOR}"
     else
-        echo "${test_folder}: FAIL"
+        echo -e "${GREEN}${test_folder}: FAIL${NO_COLOR}"
     fi
 done;
