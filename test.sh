b1b6dda26f010f3c76cb791524f13ce4a499f9ae
Yogesh Dhamija <ydhamija96@gmail.com>
2022-02-20 11:04:52 -0600

    test does not run in terminal

--- test.sh
+++ test.sh
@@ -7,11 +7,7 @@ GREEN='\033[0;32m'
 NO_COLOR='\033[0m'
 
 for test_folder in test/*; do
-    vim \
-        -Nu ./.vim/vimrc.local \
-        -c '' \
-        -s ./${test_folder}/when.vim \
-        ./${test_folder}/given.txt
+    vim --not-a-term -Nu ./.vim/vimrc.local -c '' -s ./${test_folder}/when.vim ./${test_folder}/given.txt > /dev/null
 
     if cmp --silent -- ./${test_folder}/expected.txt ./${test_folder}/actual.txt; then
         echo -e "${GREEN}${test_folder}: PASS${NO_COLOR}"
