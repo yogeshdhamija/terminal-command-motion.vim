69f4fc317700b3ce5c74efae27997e1e0378b178
Yogesh Dhamija <ydhamija96@gmail.com>
2022-06-05 22:42:04 -0500

    test name appears before running test

--- test.sh
+++ test.sh
@@ -10,23 +10,22 @@ for test_folder in test/*; do
     programs=( 'nvim -nN --headless' 'vim -nN --not-a-term' )
 
     for program in "${programs[@]}"; do
+        program_name=$(echo "${program}" | awk '{print $1}')
+        echo -ne "${program_name} ${test_folder} ... "
+
         rm "./${test_folder}/actual.txt" > /dev/null 2>&1
         ${program} -u ./.vim/vimrc.local -c '' -s ./${test_folder}/when.vim ./${test_folder}/given.txt > /dev/null 2>&1
 
-        program_name=$(echo "${program}" | awk '{print $1}')
-
         if cmp --silent -- ./${test_folder}/expected.txt ./${test_folder}/actual.txt; then
-            echo -e "${GREEN}PASS${NO_COLOR} ${program_name} ${test_folder}"
+            echo -e "${GREEN}PASS${NO_COLOR}"
         else
-            echo -e "${RED}FAIL${NO_COLOR} ${program_name} ${test_folder}"
+            echo -e "${RED}FAIL${NO_COLOR}"
             echo ""
-            echo "EXPECTED:"
+            echo -e "${GREEN}EXPECTED:${NO_COLOR}"
             cat ./${test_folder}/expected.txt
-            echo ""
-            echo "ACTUAL:"
+            echo -e "${RED}ACTUAL:${NO_COLOR}"
             cat ./${test_folder}/actual.txt
-            echo ""
-            echo ""
+            echo "-------------------------"
 
             any_failed=1
         fi
